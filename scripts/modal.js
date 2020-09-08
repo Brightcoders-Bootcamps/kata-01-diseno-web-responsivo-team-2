9984bb4d63957773f1247d26c10f1134933fd9c6
ediazjz <ediazjz@outlook.com>
2020-09-08 14:14:10 -0500

    kata-01 Statistics Deatails Added and Closing Menu
    Co-authored-by: danielasotomayor99 <danysotomayor99@gmail.com>
    Co-authored-by: KarinaDell <kdelgado@ucol.mx>

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,11 +2,11 @@ const modal = document.getElementById("modal");
 const button = document.getElementById("burguerMenu");
 
 button.onclick = function() {
-    modal.style.display = "flex";
+    modal.style.display = "block";
 }
 
 window.onclick = function(event){
     if (event.target == modal) {
         modal.style.display = "none";
-    }
+   }
 }
