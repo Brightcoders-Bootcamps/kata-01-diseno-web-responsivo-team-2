585058a987e280017e221ec7cefca2ad73814246
ediazjz <ediazjz@outlook.com>
2020-09-10 15:37:27 -0500

    kata-01 Shortened Link Template Alignment Details 1/2

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-const shortener =  document.getElementById("shorten");
+const shortener =  document.getElementById("gridtwo");
 const input = document.getElementById("input");
 const button = document.getElementById("shortenIt");
 
@@ -12,19 +12,19 @@ function addShortLink(long, short) {
     const item = `<div class="shortened-card">
                     <span class="long-link left m-shortened">${long}</span>
                     <hr>
-                    <span class="short-link left m-shortened">${short}</span>
+                    <span class="short-link left m-shortened">https://rel.ink/${short}</span>
                     <a class="shorten_button button center m-shortened">Copy</a>
                 </div>`;
     
-    const position = "afterend";
+    const position = "afterbegin";
     shortener.insertAdjacentHTML(position, item);
     input.value = "";
 }
 
 button.addEventListener("click", function(event){
     callApi()
-        .then(data => {debugger; addShortLink(input.value, data['hashid'])})
-        .catch(error => {debugger; console.error('Error:', error)});
+        .then(data => addShortLink(input.value, data['hashid']))
+        .catch(error => console.error('Error:', error));
 });
 
 
@@ -47,41 +47,3 @@ function callApi() {
 }
 
 
-// function callApi() {
-      
-//   let data = {url: input.value};
-//   fetch(api_url, {
-//   method: 'POST', 
-//   body: JSON.stringify(data), 
-//   headers:{
-//       'Content-Type': 'application/json'
-//   }
-//   }).then(res => res.json())
-//   .then(data => obj = data)
-//   .catch(error => console.error('Error:', error))
-//   .then(response => console.log('Success:', obj));
-
-// }
-
-
-
-// function callApi() {
-      
-//   let data = {url: input.value};
-//   fetch(api_url, {
-//   method: 'POST', 
-//   body: JSON.stringify(data), 
-//   headers:{
-//       'Content-Type': 'application/json'
-//   }
-//   }).then(function(response) {
-//      response.json();
-//   }) 
-//   .catch(error => console.error('Error:', error))
-//   .then(response => console.log('Success:', response));
-// }
-
-// const element = event.target;
-
-// element.innerHTML = "Copied!";
-// element.parentNode.querySelect(".long-link");
