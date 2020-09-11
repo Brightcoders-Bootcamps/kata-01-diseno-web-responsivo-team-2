284d8d248f7ae225ba553b7e6c606f895570dc44
ediazjz <ediazjz@outlook.com>
2020-09-11 17:39:00 -0500

    kata-01 Shortened Link Dynamic Template 3/3 Extra Details, Copy to Clipboard and LocalStorage Full
    Co-authored-by: danielasotomayor99 <danysotomayor99@gmail.com>
    Co-authored-by: KarinaDell <kdelgado@ucol.mx>

--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,54 @@
-const shortener =  document.getElementById("gridtwo");
+const containerLinks =  document.getElementById("containerLinks");
 const input = document.getElementById("input");
 const button = document.getElementById("shortenIt");
 const label = document.getElementById("labelform");
-const error = "error";
-
+const clear = document.getElementById("logoHeader");
 const api_url = 'https://rel.ink/api/links/';
-let LIST = [];
 
+let LIST;
+
+//Local Storage Rendering
+let saved = localStorage.getItem("shortenLinks");
+
+if(saved){
+    LIST = JSON.parse(saved);
+    loadList(LIST); 
+}else{
+    LIST = [];
+}
+
+//Loads every item of the localStorage and renders it to the user
+function loadList(array){
+    array.forEach(function(item){
+        addShortLink(item.url, item.hashid);
+    });
+}
+
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+});
+
+
+
+//Main function, it injects the HTML Template of a card and clear the error state of input
 function addShortLink(long, short) {
-    
-    if(!long){ return; }
-
     const item = `<div class="shortened-card">
                     <span class="long-link left m-shortened">${long}</span>
                     <hr>
-                    <span class="short-link left m-shortened">https://rel.ink/${short}</span>
-                    <a class="button center m-shortened copy-button">Copy</a>
+                    <input class="short-link left m-shortened" value="https://rel.ink/${short}" disabled>
+                    <input class="button center m-shortened copy-button" type="button" value="Copy">
                 </div>`;
-    
     const position = "afterbegin";
-    shortener.insertAdjacentHTML(position, item);
+
+    input.classList.remove("error");
+    label.classList.remove("labelform");
+    containerLinks.insertAdjacentHTML(position, item);
     input.value = "";
 }
-function validate(long, short) {
-    if(!long){ 
-        const item = 
-        label.style.visibility="visible";
-        input.classList.add(error);
-        
-    input.value = "";
-    }
 
-}
-button.addEventListener("click", function(event){
-    callApi()
-        .then(data => addShortLink(input.value, data['hashid']))
-        .catch(error => console.error('Error:', error));
-        validate();
-});
 
+//Function to call the Relink API
 function callApi() {
     let data = {url: input.value};
     return fetch(api_url, {
@@ -54,7 +64,51 @@ function callApi() {
         }
         throw new Error(response.statusText);
     })
-   
 }
 
+
+//Function to check if input is empty
+function validate() {
+    if(input.value == ""){ 
+        input.classList.add(error);
+        label.classList.add("is-visible");
+    }
+}
+
+
+//Function to copy the value of selected element to Clipboard
+function copyLink(element) {
+
+    navigator.clipboard.writeText(element).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+      }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+      });
+}
+
+
+// Listeners
+
+//Main listener, used to send the data from the input and call the API
+//Saving the returned data to localStorage trhough push in LIST
+button.addEventListener("click", function(event){
+    validate();
+    callApi()
+        .then(data => {
+            LIST.push(data);
+            localStorage.setItem("shortenLinks", JSON.stringify(LIST));
+            addShortLink(input.value, data['hashid']);
+        })
+        .catch(error => console.error('Error:', error));
+});
+
+//Listener to select the button and shortened link GENERATED DYNAMICALLY
+containerLinks.addEventListener("click", function(event){
+    const element = event.target;
+    const inputLink = element.parentNode.querySelector(".short-link");
+    console.log(element);
+    copyLink(inputLink.value);
+
+    element.value = "Copied!";
+});
 
